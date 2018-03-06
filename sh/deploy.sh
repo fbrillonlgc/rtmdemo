4894c4a5ea669f70a8860cb9d5bb7184653dcf70
centos <centos@rtmdemo.local>
2018-03-06 14:47:27 -0500

    Remove volume mapping, no longer used.

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -9,7 +9,5 @@ docker pull fbrillon/nifi-rtmdemo
 docker run \
 	--detach \
 	--publish 8080-8081:8080-8081 \
-	--mount type=bind,source=/lgc/share/,target=/lgc/share/,readonly \
-	--mount type=bind,source=/lgc/nifi/,target=/lgc/nifi/ \
 	--name nifi-rtmdemo \
 	fbrillon/nifi-rtmdemo
