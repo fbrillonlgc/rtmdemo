dac8db72e70c883dda12f7151fef8e2bdf0c6e6d
centos <centos@rtmdemo.local>
2018-03-08 12:21:22 -0500

    Single volume with only generated data, no software or configuration

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -8,6 +8,5 @@ docker run \
 	--detach \
 	--publish 8080:8080 \
 	--name nifi-rtmdemo \
-	--mount source=nifi-rtmdemo-vol,destination=/opt/nifi/nifi-1.5.0.3.1.0.0-564/ \
 	--mount source=lgc-rtmdemo-vol,destination=/home/nifi/lgc/ \
 	fbrillon/nifi-rtmdemo
