0eba081596f103652aaa6be04b596a7bd52898ce
centos <centos@rtmdemo.local>
2018-03-08 11:51:00 -0500

    Better error handling

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
+docker stop nifi-rtmdemo || true
+docker rm nifi-rtmdemo || true
+
 set -e
-
-mkdir -p /lgc/share/
-mkdir -p /lgc/nifi/
-
-docker stop nifi-rtmdemo
-
-docker rm nifi-rtmdemo
-
 docker pull fbrillon/nifi-rtmdemo
-
 docker run \
 	--detach \
 	--publish 8080:8080 \
