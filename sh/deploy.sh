4d843d52d7ccfdc51132f74f19816d4626e3c09f
centos <centos@rtmdemo.local>
2018-03-08 08:23:09 -0500

    Add lgc-rtmdemo-vol volume Remove port 8081

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -12,7 +12,8 @@ docker pull fbrillon/nifi-rtmdemo
 
 docker run \
 	--detach \
-	--publish 8080-8081:8080-8081 \
+	--publish 8080:8080 \
 	--name nifi-rtmdemo \
-	--mount source=nifi-rtmdemo-vol,destination=/home/nifi/nifi/ \
+	--mount source=nifi-rtmdemo-vol,destination=/opt/nifi/nifi-1.5.0.3.1.0.0-564/ \
+	--mount source=lgc-rtmdemo-vol,destination=/home/nifi/lgc/ \
 	fbrillon/nifi-rtmdemo
